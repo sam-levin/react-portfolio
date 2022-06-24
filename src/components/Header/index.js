72a1e17d2600830100c8c9c1e2f11120bebffa99
sam-levin <sam.ky.levin@gmail.com>
2022-06-24 15:49:00 -0700

    cards

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,6 @@
 import react from "react";
 
 const Header = () => {
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-      }
-    const categories = [
-        {
-          name: ""
-        },
-        { name: "portraits", description: "Portraits of people in my life" },
-        {
-          name: "landscape",
-          description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-      ];
 
     return (
         <header className="flex-auto">
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,22 +2,22 @@ import react from "react";
 const Projects = () => {
     return (
         <div> 
-            <div>
+            <div className="card">
                 <h4>
                     Social Network API
                 </h4>
             </div>
-            <div>
+            <div className="card">
                 <h4>
                     Front End Project
                 </h4>
             </div>
-            <div>
+            <div className="card">
                 <h4>
                     SQL Database
                 </h4>
             </div>
-            <div>
+            <div className="card">
                 <h4>
                     MERN Stack Project
                 </h4>
