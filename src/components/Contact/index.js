d0c47b45a29df47d2d59994e3053563243c4d637
sam-levin <sam.ky.levin@gmail.com>
2022-06-24 17:18:56 -0700

    adding contact functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,63 @@
-import React from "react";
+import {React, useState} from "react";
+import { validateEmail } from '../../utils/helpers';
 
-const Contact = () => {
-   return ( 
-       <h1>Sam's Portorfolio</h1>
-   )
-    }
+function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
 
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+              setErrorMessage(`${e.target.name} is required.`);
+            } else {
+              setErrorMessage('');
+            }
+          }  
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+          }     }
+      
+      console.log(formState);
+    //jsx
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+      }
+    return (
+        <section>
+            <h1>Contact Me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                        )}
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
+}
 
 export default Contact;
