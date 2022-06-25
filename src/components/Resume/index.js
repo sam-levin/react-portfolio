5f9c2e610d58bb479075c85d339b464beed511d5
sam-levin <sam.ky.levin@gmail.com>
2022-06-24 19:21:11 -0700

    resume download capability

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Pdf from '../../assets/resume/Sam-Levin-Resume.pdf'
 
 const Resume = () => {
     return (
         <div>
             <h2>Resume and Skills</h2>
-            <a>Click here to download my resume!</a>
+            <a href={Pdf} target="blank">Click here to download my resume!</a>
             <h3>Skills</h3>
             <ul>
                 <li>MongoDB</li>
